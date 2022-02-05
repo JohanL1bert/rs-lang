f9b531d433577533ceca62ede591fdb19ef70790
vsmaliakou <vsmaliakou@axmit.com>
2022-02-05 12:01:19 +0300

    feat: add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HomePage } from 'app/pages/HomePage';
+import { TextbookPage } from 'app/pages/TextbookPage';
 
 export const App: React.FC = () => {
-  return <div className="app">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/textbook" element={<TextbookPage />} />
+      </Routes>
+    </Router>
+  );
 };
