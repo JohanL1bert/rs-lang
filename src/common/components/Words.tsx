4593cc2cb425897f4cbe542a00a593ecdaf63a67
vsmaliakou <vsmaliakou@axmit.com>
2022-02-13 20:36:47 +0300

    feat: RS-14 Restyling Textbook page

--- src/common/components/Words.tsx
+++ src/common/components/Words.tsx
@@ -14,8 +14,30 @@ interface IComponentProps {
 export const Words: React.FC<IComponentProps> = (props) => {
   const { group, words, page, setPage, isVisibleTranslation } = props;
   const [word, setWord] = useState<IWord>(words[0]);
+  const [audio, setAudio] = useState<HTMLAudioElement[]>([
+    new Audio(`${basePath}/${word.audio}`),
+    new Audio(`${basePath}/${word.audioMeaning}`),
+    new Audio(`${basePath}/${word.audioExample}`),
+  ]);
   const pageSize = 30;
   const bg = `bg-${group}`;
+
+  const changeWord = (wd: IWord) => {
+    setWord(wd);
+    setAudio([
+      new Audio(`${basePath}/${wd.audio}`),
+      new Audio(`${basePath}/${wd.audioMeaning}`),
+      new Audio(`${basePath}/${wd.audioExample}`),
+    ]);
+  };
+
+  const play = (idx: number) => {
+    audio.forEach((item) => {
+      item.pause();
+    });
+
+    audio[idx].play();
+  };
 
   return (
     <div className="words">
@@ -25,7 +47,7 @@ export const Words: React.FC<IComponentProps> = (props) => {
           <div className="words__section">
             {words.map((wd, index) => {
               return (
-                <div key={index} className={`words__item ${wd.word === word.word && bg}`} onClick={() => setWord(wd)}>
+                <div key={index} className={`words__item ${wd.word === word.word && bg}`} onClick={() => changeWord(wd)}>
                   {wd.word}
                 </div>
               );
@@ -39,17 +61,25 @@ export const Words: React.FC<IComponentProps> = (props) => {
               <p className="words__presentation_word">{word.word}</p>
               <p className={`words__presentation_translate ${!isVisibleTranslation && 'unvisible'}`}>{word.wordTranslate}</p>
               <div className="words__presentation_transcription">
+                <button className="words__presentation_play" onClick={() => play(0)}></button>
                 <span>{word.transcription}</span>
-                <button className="words__presentation_play"></button>
               </div>
             </div>
             <div className="words__presentation_img" style={{ backgroundImage: `url(${basePath}/${word.image})` }}></div>
           </div>
           <p className="words__presentation_meaning">Значение</p>
-          <p>{word.textMeaning}</p>
+          <div className="words__presentation_transcription">
+            <button className="words__presentation_play" onClick={() => play(1)}></button>
+            {/* eslint-disable-next-line */}
+            <span dangerouslySetInnerHTML={{ __html: word.textMeaning }}></span>
+          </div>
           <p className={!isVisibleTranslation ? 'unvisible' : ''}>{word.textMeaningTranslate}</p>
           <p className="words__presentation_meaning">Перевод</p>
-          <p>{word.textExample}</p>
+          <div className="words__presentation_transcription">
+            <button className="words__presentation_play" onClick={() => play(2)}></button>
+            {/* eslint-disable-next-line */}
+            <span dangerouslySetInnerHTML={{ __html: word.textExample }}></span>
+          </div>
           <p className={!isVisibleTranslation ? 'unvisible' : ''}>{word.textExampleTranslate}</p>
         </div>
       </div>
