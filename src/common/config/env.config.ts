f090f01f2920a5bf77bfe827f8db328a29430e25
Johan <grimmerw968@gmail.com>
2023-06-06 19:59:40 +0300

    fix: change path to heroku

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -2,4 +2,6 @@
 
 export const envConfig = (process.env || (window as any).env) as { REACT_APP_SENTRY_DSN: string; NODE_ENV: string };
 
-export const basePath = 'https://react-learwords.herokuapp.com';
+export const basePath = 'https://react-words.herokuapp.com';
+// defualt path who stay https://react-learwords.herokuapp.com
+// new path what i create https://react-words.herokuapp.com/
